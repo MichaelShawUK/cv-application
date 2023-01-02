950f0e141d50f941d28f742aa32c7f888f361a46
MichaelShawUK <MichaelShawUK@gmail.com>
2023-01-02 19:20:03 +0000

    Refactor Header.js to use function components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,77 +1,55 @@
-import { Component } from 'react';
-import styled from 'styled-components';
+import { useState } from "react";
+import styled from "styled-components";
 
 const StyledHeader = styled.div`
   cursor: pointer;
-
   &:hover {
     text-shadow: 0px 0px 5px black;
   }
-`
+`;
 
-class Header extends Component {
+const Header = () => {
+  const [name, setName] = useState("JOE Bloggs");
+  const [title, setTitle] = useState("Web Developer");
+  const [edit, setEdit] = useState(false);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "JOE Bloggs",
-      title: "Web Developer",
-      edit: false,
-    }
-    this.handleClick = this.handleClick.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  function handleClick() {
+    setEdit(true);
   }
 
-  handleClick = () => {
-    this.setState({
-      edit: true,
-    })
-  }
-
-  handleSubmit = (e) => {
+  function handleSubmit(e) {
     e.preventDefault();
-    this.setState({
-      name: e.target[0].value,
-      title: e.target[1].value,
-    })
+    setName(e.target[0].value);
+    setTitle(e.target[1].value);
 
-    if (e.target[0].value || e.target[1].value) {
-      this.setState({edit: false});
-    } else {
-      this.setState({edit: true});
-    }
+    if (e.target[0].value || e.target[1].value) setEdit(false);
+    else setEdit(true);
   }
 
-  render() {
-    const { edit, name, title } = this.state;
-
-    if (edit) {
-      return (
-        <StyledHeader id="header-input">
-          <form onSubmit={this.handleSubmit}>
-            <div className="grid-container">
-              <input
-                defaultValue={name}
-                placeholder="Name"
-              ></input>
-              <input
-                defaultValue={title}
-                placeholder="Profession"
-              ></input>
-              <button>UPDATE</button>
-            </div>
-          </form>
-        </StyledHeader>
-      )
-    }
-
+  const DisplayHeader = () => {
     return (
-      <StyledHeader onClick={() => this.handleClick()}>
+      <StyledHeader onClick={handleClick}>
         <h1>{name}</h1>
         <h3>{title}</h3>
       </StyledHeader>
-    )
-  }
-}
+    );
+  };
+
+  const EditHeader = () => {
+    return (
+      <StyledHeader id="header-input">
+        <form onSubmit={handleSubmit}>
+          <div className="grid-container">
+            <input defaultValue={name} placeholder="Name"></input>
+            <input defaultValue={title} placeholder="Profession"></input>
+            <button>UPDATE</button>
+          </div>
+        </form>
+      </StyledHeader>
+    );
+  };
+
+  return edit ? <EditHeader /> : <DisplayHeader />;
+};
 
 export default Header;
